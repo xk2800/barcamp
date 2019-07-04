cd14ca57ae5c6ecb49ac61f6d14e31969684fcd3
Evangelista Grace <evangrace98@gmail.com>
2019-07-05 02:59:27 +0800

    updated speaker form

--- js/submit-form.js
+++ js/submit-form.js
@@ -1,6 +1,7 @@
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwzcrherJ-FkfGVIqpglnkm6HwPo3Mp94LymeEb7dJWOCHY9NE/exec'
 const form = document.forms['submit-to-google-sheet']
+const otherCategory =  document.querySelector('.others');
 
 form.addEventListener('submit', e => {
     e.preventDefault()
@@ -9,6 +10,11 @@ form.addEventListener('submit', e => {
         document.querySelector(".message").innerHTML = "Success! Thank you for your submission :)";
         console.log('Success!', response);
         document.getElementById("form").reset();
+        otherCategory.style.display = 'none';
     })
     .catch(error => console.error('Error!', error.message))
 });
+
+function showOthers(){
+    otherCategory.style.display = 'block';
+}
